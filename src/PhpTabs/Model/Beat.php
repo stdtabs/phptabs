0a73c1c803eadcfa4632982e8818785805a1645e
Landrok <Landrok@users.noreply.github.com>
2016-04-30 00:57:15 +0200

    Beat indent

--- src/PhpTabs/Model/Beat.php
+++ src/PhpTabs/Model/Beat.php
@@ -17,7 +17,7 @@ class Beat
   private $voices;
   private $stroke;
 
-	public function __construct()
+  public function __construct()
   {
     $this->start = Duration::QUARTER_TIME;
     $this->stroke = new Stroke();
@@ -36,120 +36,127 @@ class Beat
     return $this->measure;
   }
 	
-	public function setMeasure(Measure $measure)
+  public function setMeasure(Measure $measure)
   {
-		$this->measure = $measure;
-	}
+    $this->measure = $measure;
+  }
 	
-	public function getStart()
+  public function getStart()
   {
-		return $this->start;
-	}
-	
-	public function setStart($start)
-  {
-		$this->start = $start;
-	}
-	
-	public function setVoice($index, Voice $voice)
-  {
-		if($index >= 0)
-    {
-			$this->voices[$index] = $voice;
-			$this->voices[$index]->setBeat($this);
-		}
-	}
-	
-	public function getVoice($index)
-  {
-		if($index >= 0 && $index < count($this->voices))
-    {
-			return $this->voices[$index];
-		}
-		return null;
-	}
-	
-	public function countVoices()
-  {
-		return count($this->voices);
-	}
-	
-	public function setChord(Chord $chord)
-  {
-		$this->chord = $chord;
-		$this->chord->setBeat($this);
-	}
-	
-	public function getChord()
-  {
-		return $this->chord;
-	}
-	
-	public function removeChord()
-  {
-		$this->chord = null;
-	}
-	
-	public function getText()
-  {
-		return $this->text;
-	}
-	
-	public function setText(Text $text)
-  {
-		$this->text = $text;
-		$this->text->setBeat($this);
-	}
-	
-	public function removeText()
-  {
-		$this->text = null;
-	}
-	
-	public function isChordBeat()
-  {
-		return $this->chord != null;
-	}
-	
-	public function isTextBeat()
-  {
-		return $this->text != null;
-	}
-	
-	public function getStroke()
-  {
-		return $this->stroke;
-	}
-	
-	public function isRestBeat()
-  {
-		for($v = 0; $v < $this->countVoices(); $v++)
-    {
-			$voice = $this->getVoice($v);
-			if(!$voice->isEmpty() && !$voice->isRestVoice())
-      {
-				return false;
-			}
-		}
+    return $this->start;
+  }
 
-		return true;
-	}
-	
-	public function __clone()
+  public function setStart($start)
   {
-		$beat = new Beat();
-		$beat->setStart($this->getStart());
-		$beat->getStroke()->copyFrom($this->getStroke());
-		for ($i = 0; $i < count($this->voices); $i++)
+    $this->start = $start;
+  }
+
+  public function setVoice($index, Voice $voice)
+  {
+    if($index >= 0)
     {
-			$beat->setVoice($i, clone $this->voices[$i]);
-		}
-		if($this->chord != null)
-			$beat->setChord(clone $this->chord);
-		
-		if($this->text != null)
-			$beat->setText(clone $this->text);
-		
-		return $beat;
-	}
+      $this->voices[$index] = $voice;
+      $this->voices[$index]->setBeat($this);
+    }
+  }
+
+  public function getVoice($index)
+  {
+    if($index >= 0 && $index < count($this->voices))
+    {
+      return $this->voices[$index];
+    }
+    return null;
+  }
+
+  public function countVoices()
+  {
+    return count($this->voices);
+  }
+
+  public function setChord(Chord $chord)
+  {
+    $this->chord = $chord;
+    $this->chord->setBeat($this);
+  }
+
+  public function getChord()
+  {
+    return $this->chord;
+  }
+
+  public function removeChord()
+  {
+    $this->chord = null;
+  }
+
+  public function getText()
+  {
+    return $this->text;
+  }
+
+  public function setText(Text $text)
+  {
+    $this->text = $text;
+    $this->text->setBeat($this);
+  }
+
+  public function removeText()
+  {
+    $this->text = null;
+  }
+
+  public function isChordBeat()
+  {
+    return $this->chord != null;
+  }
+
+  public function isTextBeat()
+  {
+    return $this->text != null;
+  }
+
+  public function getStroke()
+  {
+    return $this->stroke;
+  }
+
+  public function isRestBeat()
+  {
+    for($v = 0; $v < $this->countVoices(); $v++)
+    {
+      $voice = $this->getVoice($v);
+
+      if(!$voice->isEmpty() && !$voice->isRestVoice())
+      {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public function __clone()
+  {
+    $beat = new Beat();
+    $beat->setStart($this->getStart());
+    $beat->getStroke()->copyFrom($this->getStroke());
+
+    for ($i = 0; $i < count($this->voices); $i++)
+    {
+      $beat->setVoice($i, clone $this->voices[$i]);
+    }
+    
+    if($this->chord != null)
+    {
+      $beat->setChord(clone $this->chord);
+    }
+
+    if($this->text != null)
+    {
+      $beat->setText(clone $this->text);
+    }
+
+    return $beat;
+  }
 }
