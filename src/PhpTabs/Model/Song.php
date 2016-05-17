b6eee633f3cc9224e1969f8e58ffa4f6809bb212
Landrok <Landrok@users.noreply.github.com>
2016-05-18 01:37:57 +0200

    Song optimization

--- src/PhpTabs/Model/Song.php
+++ src/PhpTabs/Model/Song.php
@@ -2,130 +2,8 @@
 
 namespace PhpTabs\Model;
 
-
-class Song
+class Song extends SongBase
 {
-  private $name;
-  private $artist;
-  private $album;
-  private $author;
-  private $date;
-  private $copyright;
-  private $writer;
-  private $transcriber;
-  private $comments;
-  private $tracks = array();
-  private $measureHeaders = array();
-  private $channels = array();
-
-  public function __construct()
-  {
-    $this->name = '';
-    $this->artist = '';
-    $this->album = '';
-    $this->author = '';
-    $this->date = '';
-    $this->copyright = '';
-    $this->writer = '';
-    $this->transcriber = '';
-    $this->comments = '';
-  }
-
-  public function getName()
-  {
-    return $this->name;
-  }
-
-  public function setName($name)
-  {
-    $this->name = $name;
-  }
-
-  public function getAlbum()
-  {
-    return $this->album;
-  }
-
-  public function setAlbum($album)
-  {
-    $this->album = $album;
-  }
-
-  public function getAuthor()
-  {
-    return $this->author;
-  }
-
-  public function setAuthor($author)
-  {
-    $this->author = $author;
-  }
-
-  public function getArtist()
-  {
-    return $this->artist;
-  }
-
-  public function setArtist($artist)
-  {
-    $this->artist = $artist;
-  }
-
-  public function getDate()
-  {
-    return $this->date;
-  }
-
-  public function setDate($date)
-  {
-    $this->date = $date;
-  }
-
-  public function getCopyright()
-  {
-    return $this->copyright;
-  }
-
-  public function setCopyright($copyright)
-  {
-    $this->copyright = $copyright;
-  }
-
-  public function getWriter()
-  {
-    return $this->writer;
-  }
-
-  public function setWriter($writer)
-  {
-    $this->writer = $writer;
-  }
-
-  public function getTranscriber()
-  {
-    return $this->transcriber;
-  }
-
-  public function setTranscriber($transcriber)
-  {
-    $this->transcriber = $transcriber;
-  }
-
-  public function getComments()
-  {
-    return $this->comments;
-  }
-
-  public function setComments($comments)
-  {
-    $this->comments = $comments;
-  }
-
-  public function countMeasureHeaders()
-  {
-    return count($this->measureHeaders);
-  }
-
   public function addMeasureHeader(MeasureHeader $measureHeader)
   {
     $measureHeader->setSong($this);
@@ -148,17 +26,12 @@ class Song
     return $this->measureHeaders;
   }
 
-  public function countTracks()
-  {
-    return count($this->tracks);
-  }
-
   public function addTrack(Track $track)
   {
     $this->tracks[$this->countTracks()] = $track;
   }
 
-  public function moveTrack($index,Track $track)
+  public function moveTrack($index, Track $track)
   {
     $this->removeTrack($track);
     $this->tracks[$index] = $track;
@@ -188,18 +61,13 @@ class Song
     return $this->tracks;
   }
 
-  public function countChannels()
-  {
-    return count($this->channels);
-  }
-
   public function addChannel($index, $channel = null)
   {
     if($index instanceof Channel)
     {
       $this->channels[] = $index;
     }
-    else
+    else if(is_int($index))
     {
       array_splice($this->channels, $index, 0, $channel);
     }
@@ -232,60 +100,10 @@ class Song
     return $this->channels;
   }
 
-  public function isEmpty()
-  {
-    return ($this->countMeasureHeaders() == 0 || $this->countTracks() == 0);
-  }
-
-  public function clear()
-  {
-    $tracks = $this->getTracks();
-    foreach($tracks as $k => $v)
-    {
-      $tracks[$k]->clear();
-    }
-
-    $this->tracks = array();
-    $this->channels = array();
-    $this->measureHeaders = array();
-  }
-
   public function __clone()
   {
     $song = new Song();
     $song->copyFrom($this);
     return $song;
-  }
-
-  public function copyFrom(Song $song)
-  {
-    $this->clear();
-    $this->setName($song->getName());
-    $this->setArtist($song->getArtist());
-    $this->setAlbum($song->getAlbum());
-    $this->setAuthor($song->getAuthor());
-    $this->setDate($song->getDate());
-    $this->setCopyright($song->getCopyright());
-    $this->setWriter($song->getWriter());
-    $this->setTranscriber($song->getTranscriber());
-    $this->setComments($song->getComments());
-
-    $headers = $song->getMeasureHeaders();
-    foreach($headers as $header)
-    {
-      $this->addMeasureHeader(clone $header);
-    }
-
-    $channels = $song->getChannels();
-    foreach($channels as $channel)
-    {
-      $this->addChannel(clone $channel);
-    }
-
-    $tracks = $song->getTracks();
-    foreach($tracks as $track)
-    {
-      $this->addTrack(clone $track);
-    }
   }
 }
---
+++ src/PhpTabs/Model/SongBase.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace PhpTabs\Model;
+
+class SongBase
+{
+  protected $name;
+  protected $artist;
+  protected $album;
+  protected $author;
+  protected $date;
+  protected $copyright;
+  protected $writer;
+  protected $transcriber;
+  protected $comments;
+  protected $tracks = array();
+  protected $measureHeaders = array();
+  protected $channels = array();
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+
+  public function getAlbum()
+  {
+    return $this->album;
+  }
+
+  public function setAlbum($album)
+  {
+    $this->album = $album;
+  }
+
+  public function getAuthor()
+  {
+    return $this->author;
+  }
+
+  public function setAuthor($author)
+  {
+    $this->author = $author;
+  }
+
+  public function getArtist()
+  {
+    return $this->artist;
+  }
+
+  public function setArtist($artist)
+  {
+    $this->artist = $artist;
+  }
+
+  public function getDate()
+  {
+    return $this->date;
+  }
+
+  public function setDate($date)
+  {
+    $this->date = $date;
+  }
+
+  public function getCopyright()
+  {
+    return $this->copyright;
+  }
+
+  public function setCopyright($copyright)
+  {
+    $this->copyright = $copyright;
+  }
+
+  public function getWriter()
+  {
+    return $this->writer;
+  }
+
+  public function setWriter($writer)
+  {
+    $this->writer = $writer;
+  }
+
+  public function getTranscriber()
+  {
+    return $this->transcriber;
+  }
+
+  public function setTranscriber($transcriber)
+  {
+    $this->transcriber = $transcriber;
+  }
+
+  public function getComments()
+  {
+    return $this->comments;
+  }
+
+  public function setComments($comments)
+  {
+    $this->comments = $comments;
+  }
+
+  public function countChannels()
+  {
+    return count($this->channels);
+  }
+
+  public function countTracks()
+  {
+    return count($this->tracks);
+  }
+
+  public function countMeasureHeaders()
+  {
+    return count($this->measureHeaders);
+  }
+
+  public function clear()
+  {
+    $tracks = $this->getTracks();
+    foreach($tracks as $k => $track)
+    {
+      $track->clear();
+    }
+
+    $this->tracks = array();
+    $this->channels = array();
+    $this->measureHeaders = array();
+  }
+
+  public function isEmpty()
+  {
+    return ($this->countMeasureHeaders() == 0 || $this->countTracks() == 0);
+  }
+
+  public function copyFrom(Song $song)
+  {
+    $this->clear();
+    $this->setName($song->getName());
+    $this->setArtist($song->getArtist());
+    $this->setAlbum($song->getAlbum());
+    $this->setAuthor($song->getAuthor());
+    $this->setDate($song->getDate());
+    $this->setCopyright($song->getCopyright());
+    $this->setWriter($song->getWriter());
+    $this->setTranscriber($song->getTranscriber());
+    $this->setComments($song->getComments());
+
+    $headers = $song->getMeasureHeaders();
+    foreach($headers as $header)
+    {
+      $this->addMeasureHeader(clone $header);
+    }
+
+    $channels = $song->getChannels();
+    foreach($channels as $channel)
+    {
+      $this->addChannel(clone $channel);
+    }
+
+    $tracks = $song->getTracks();
+    foreach($tracks as $track)
+    {
+      $this->addTrack(clone $track);
+    }
+  }
+}
