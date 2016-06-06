8b55e3092dd44967f9ce7f809448dcf8d755978b
Landrok <Landrok@users.noreply.github.com>
2016-06-06 23:46:09 +0200

    Update a variable name

--- src/PhpTabs/Reader/GuitarPro/Helper/GuitarPro4Chord.php
+++ src/PhpTabs/Reader/GuitarPro/Helper/GuitarPro4Chord.php
@@ -21,6 +21,7 @@ class GuitarPro4Chord extends AbstractReader
     {
       $chord->setName($this->reader->readStringByteSizeOfInteger());
       $chord->setFirstFret($this->reader->readInt());
+
       if($chord->getFirstFret() != 0)
       {
         for ($i = 0; $i < 6; $i++)
@@ -35,6 +36,7 @@ class GuitarPro4Chord extends AbstractReader
       $chord->setName($this->reader->readStringByte(21));
       $this->reader->skip(4);
       $chord->setFirstFret($this->reader->readInt());
+
       for ($i = 0; $i < 7; $i++)
       {
         $this->readFret($chord, $i);
@@ -49,13 +51,13 @@ class GuitarPro4Chord extends AbstractReader
     }
   }
 
-  private function readFret(Chord $chord, $i)
+  private function readFret(Chord $chord, $index)
   {
     $fret = $this->reader->readInt();
 
-    if($i < $chord->countStrings())
+    if($index < $chord->countStrings())
     {
-      $chord->addFretValue($i, $fret);
+      $chord->addFretValue($index, $fret);
     }
   }
 }
