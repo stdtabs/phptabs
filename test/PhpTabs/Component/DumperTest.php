4234816da7c1ca14cbce8dbaebac714847a70d64
Landrok <Landrok@users.noreply.github.com>
2017-08-01 17:08:03 +0200

    [Test] Dump types

--- test/PhpTabs/Component/DumperTest.php
+++ test/PhpTabs/Component/DumperTest.php
@@ -11,6 +11,84 @@ use PhpTabs\PhpTabs;
  */
 class DumperTest extends PHPUnit_Framework_TestCase
 {
+  public function setUp()
+  {
+    $this->tablature = new PhpTabs(
+      PHPTABS_TEST_BASEDIR 
+      . '/samples/testSimpleTab.gp5'
+    );
+  }
+
+  /**
+   * Following dumps must be a string
+   * - text
+   * - txt
+   * - yaml
+   * - yml
+   * - serialize
+   * - json
+   * - var_export
+   * - xml
+   */
+  public function testStringFormats()
+  {
+    foreach ([
+      'text',
+      'txt',
+      'yaml',
+      'yml',
+      'serialize',
+      'json',
+      'var_export',
+      'xml'
+    ] as $format
+    ) {
+      $this->assertTrue(
+        is_string(
+          $this->tablature->dump($format)
+        )
+      );
+    }
+  }
+
+  /**
+   * Following dumps must be an array
+   * - array
+   * - none
+   */
+  public function testArrayFormats()
+  {
+    foreach ([
+      'array',
+      null
+    ] as $format
+    ) {
+      $this->assertTrue(
+        is_array(
+          $this->tablature->dump($format)
+        )
+      );
+    }
+  }
+
+  /**
+   * Some dump format parameters have aliases
+   * - text = txt
+   * - yaml = yml
+   */
+  public function testAliases()
+  {
+    $this->assertSame(
+      $this->tablature->dump('text'),
+      $this->tablature->dump('txt')
+    );
+    
+    $this->assertSame(
+      $this->tablature->dump('yaml'),
+      $this->tablature->dump('yml')
+    );
+  }
+
   /**
    * @expectedException Exception
    */
