8b6ab86146f30fce1342247d291c21a44041ba88
Landrok <Landrok@users.noreply.github.com>
2017-08-30 20:35:39 +0200

    Add license information

--- src/PhpTabs/Reader/GuitarPro/Helper/GuitarPro4Note.php
+++ src/PhpTabs/Reader/GuitarPro/Helper/GuitarPro4Note.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PhpTabs package.
+ *
+ * Copyright (c) landrok at github.com/landrok
+ *
+ * For the full copyright and license information, please see
+ * <https://github.com/stdtabs/phptabs/blob/master/LICENSE>.
+ */
+
 namespace PhpTabs\Reader\GuitarPro\Helper;
 
 use PhpTabs\Music\Note;
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PhpTabs package.
+ *
+ * Copyright (c) landrok at github.com/landrok
+ *
+ * For the full copyright and license information, please see
+ * <https://github.com/stdtabs/phptabs/blob/master/LICENSE>.
+ */
+
 # Defines base dir for tests
 defined('PHPTABS_TEST_BASEDIR') || define('PHPTABS_TEST_BASEDIR', realpath(__DIR__) . '/PhpTabs');
 
