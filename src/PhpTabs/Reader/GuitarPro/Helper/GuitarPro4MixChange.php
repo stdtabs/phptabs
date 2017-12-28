a2edbea0c22cecd3c887136d54e7ce5f1576614f
Landrok <Landrok@users.noreply.github.com>
2017-12-28 08:54:25 +0100

    [QA] Improve Guitar Pro 4 Mix Change parser

--- src/PhpTabs/Reader/GuitarPro/Helper/GuitarPro4MixChange.php
+++ src/PhpTabs/Reader/GuitarPro/Helper/GuitarPro4MixChange.php
@@ -23,46 +23,22 @@ class GuitarPro4MixChange extends AbstractReader
   public function readMixChange(Tempo $tempo)
   {
     $this->reader->readByte(); //instrument
-    $volume = $this->reader->readByte();
-    $pan = $this->reader->readByte();
-    $chorus = $this->reader->readByte();
-    $reverb = $this->reader->readByte();
-    $phaser = $this->reader->readByte();
-    $tremolo = $this->reader->readByte();
+    
+    $criterias = ['volume', 'pan', 'chorus', 'reverb', 'phaser', 'tremolo'];
+    
+    foreach ($criterias as $name) {
+      $$name = $this->reader->readByte();
+    }
+
     $tempoValue = $this->reader->readInt();
 
-    if ($volume >= 0)
-    {
-      $this->reader->readByte();
+    foreach ($criterias as $name) {
+      if ($$name >= 0) {
+        $this->reader->readByte();
+      }
     }
 
-    if ($pan >= 0)
-    {
-      $this->reader->readByte();
-    }
-
-    if ($chorus >= 0)
-    {
-      $this->reader->readByte();
-    }
-
-    if ($reverb >= 0)
-    {
-      $this->reader->readByte();
-    }
-
-    if ($phaser >= 0)
-    {
-      $this->reader->readByte();
-    }
-
-    if ($tremolo >= 0)
-    {
-      $this->reader->readByte();
-    }
-
-    if ($tempoValue >= 0)
-    {
+    if ($tempoValue >= 0) {
       $tempo->setValue($tempoValue);
       $this->reader->readByte();
     }
