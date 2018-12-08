3244462f2eb44ddbe243b3caf2030445a3f28835
dem <landrok@users.noreply.github.com>
2018-12-08 16:39:31 +0100

    Fix PHPUnit TestCase

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -17,3 +17,9 @@ include_once dirname(__DIR__) . '/vendor/autoload.php';
 
 # Project-specific autoloader (Included for tests)
 include_once dirname(__DIR__) . '/src/PhpTabs/bootstrap.php';
+
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase')
+    && class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
